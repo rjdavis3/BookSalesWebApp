e78a13612992be11a462008a41b2085dbc71dade
R.J. Davis <rjdavis3@me.com>
2017-12-09 10:26:01 -0500

    Initial commit

---
+++ BookSalesWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookSalesWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
